e15c702fe1a948a9e9cdc0923812b713026af7f2
Carlos Fernández Maza <carlos@onlinevalles.com>
2023-07-08 10:42:53 +0200

    commit carlos 8 julio

--- src/errors/createJsonError.js
+++
@@ -1,9 +0,0 @@
-function createJsonError(error, res){
-    const {status, message} = error;
-    res.status(status||500)
-    res.send({
-        error: message
-    })
-}
-
-module.exports = createJsonError;
--- src/errors/throwJsonError.js
+++
@@ -1,8 +0,0 @@
-function throwJsonError(status, message){
-    const error = new Error(message)
-    error.status = status
-
-    throw error
-}
-
-module.exports = throwJsonError;
